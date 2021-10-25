923889e6ff61a25495e95d5a9a8d4c9d8f0383da
Zach Deibert <zachdeibert@gmail.com>
2021-10-25 12:50:38 -0500

    Initial commit

---
+++ uploader/src/downloader.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, const char **argv) {
+    cout << "Hello from the downloader!" << endl;
+    return EXIT_SUCCESS;
+}
---
+++ uploader/src/uploader.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, const char **argv) {
+    cout << "Hello from the uploader!" << endl;
+    return EXIT_SUCCESS;
+}
